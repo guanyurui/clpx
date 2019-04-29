558e9b0e4bc2f192bc187c8802fef80541841662
“guanyr5 <1172599025@qq.com>
2019-04-29 14:45:03 +0800

    会议管理

---
+++ src/views/meeting/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> {{ msg }}</div>
+</template>
+<script>
+// import { fetchNewsList } from '@/api/metting'
+export default {
+  data() {
+    return { msg: 'vue 模板页' }
+  }
+}
+</script>
+
+<style>
+
+</style>
